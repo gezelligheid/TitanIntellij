9d3cb5c9a803cab7630f8d5b4b50002b26479bcc
Alain Van Rijn <ayvr015@gmail.com>
2019-06-16 16:27:06 +0200

    new solar system logic works

--- src/AlternativeBody.java
+++ src/AlternativeBody.java
@@ -38,7 +38,7 @@ public class AlternativeBody {
     public Vector compute_acceleration(List<AlternativeBody> solarSystem) throws CloneNotSupportedException {
         Vector resultAcceleration = new Vector(0, 0, 0);
         for (AlternativeBody body : solarSystem
-        ) { if (!this.equals(body)){
+        ) { if (!this.name.equals(body.name)){
             final Vector distances = ((Vector) body.position.clone()).subtract(this.position);
             Vector normDistances = ((Vector) distances.clone()).normalize();
             Vector currentAcceleration = normDistances.multiply((Physics.G * body.mass) / distances.squareLength());
@@ -82,6 +82,51 @@ public class AlternativeBody {
         kVelTotal.scale((double) dt / 6);
 
         this.position.add(kPosTotal);
+        this.velocity.add(kVelTotal);
+//        this.position = ((Vector)this.position.clone()).sum((((Vector)k1Pos.clone()).sum(((Vector)k2Pos.clone())
+//                .multiply(2)).sum(((Vector)k3Pos.clone()).multiply(2)).sum((Vector)k4Pos.clone()).multiply(1/6)));
+//        this.velocity = ((Vector)this.velocity.clone()).sum((((Vector)k1Vel.clone()).sum(((Vector)k2Vel.clone())
+//                .multiply(2)).sum(((Vector)k3Vel.clone()).multiply(2)).sum((Vector)k4Vel.clone())).multiply(1/6));
+
+    }
+    /**
+     * moves the body under influence of a list of other mass objects
+     *
+     * @param dt        time step
+     * @param alternativeBodies the other mass objects
+     */
+    public void step(double dt, List<AlternativeBody> alternativeBodies) throws CloneNotSupportedException {
+        final Vector k1Pos = (Vector) this.velocity.clone(); // k1 for the change in position vector
+        final Vector k1Vel = this.compute_acceleration(alternativeBodies);// k1 for the change velocity vector
+        final Vector k2Pos = ((Vector) this.velocity.clone()).sum(((Vector) k1Vel.clone()).multiply(dt / 2));
+        AlternativeBody k1StateBody = new AlternativeBody(this.name,
+                ((Vector) this.position.clone()).sum(((Vector) k1Pos.clone()).multiply(dt / 2)),this.velocity,this.mass,this.radius);
+        final Vector k2Vel = k1StateBody.compute_acceleration(alternativeBodies);
+        final Vector k3Pos = ((Vector) this.velocity.clone()).sum(((Vector) k2Vel.clone()).multiply(dt / 2));
+        AlternativeBody k2StateBody = new AlternativeBody(this.name,
+                ((Vector) this.position.clone()).sum(((Vector) k2Pos.clone()).multiply(dt / 2)),this.velocity,this.mass,this.radius);
+        final Vector k3Vel = k2StateBody.compute_acceleration(alternativeBodies);
+        final Vector k4Pos = ((Vector) this.velocity.clone()).sum(((Vector) k3Vel.clone()).multiply(dt));
+        AlternativeBody k3StateBody = new AlternativeBody(this.name,
+                ((Vector) this.position.clone()).sum(((Vector) k3Pos.clone()).multiply(dt)),this.velocity,this.mass,this.radius);
+        final Vector k4Vel = k3StateBody.compute_acceleration(alternativeBodies);
+
+        Vector kPosTotal = new Vector(0, 0, 0);
+        Vector kVelTotal = new Vector(0, 0, 0);
+
+        kPosTotal.add(k1Pos); // Runge-Kutta coefficient sums
+        kPosTotal.add(((Vector) k2Pos.clone()).multiply(2.0));
+        kPosTotal.add(((Vector) k3Pos.clone()).multiply(2.0));
+        kPosTotal.add(k4Pos);
+        kPosTotal.scale((double) dt / 6);
+
+        kVelTotal.add(k1Vel);
+        kVelTotal.add(((Vector) k2Vel.clone()).multiply(2.0));
+        kVelTotal.add(((Vector) k3Vel.clone()).multiply(2.0));
+        kVelTotal.add(k4Vel);
+        kVelTotal.scale((double) dt / 6);
+
+        this.position.add(kPosTotal); // add the change
         this.velocity.add(kVelTotal);
 //        this.position = ((Vector)this.position.clone()).sum((((Vector)k1Pos.clone()).sum(((Vector)k2Pos.clone())
 //                .multiply(2)).sum(((Vector)k3Pos.clone()).multiply(2)).sum((Vector)k4Pos.clone()).multiply(1/6)));
--- testing/AlternativeBodyTest.java
+++ testing/AlternativeBodyTest.java
@@ -180,13 +180,97 @@ class AlternativeBodyTest {
                 new Vector(0, 0, 0), SUN_MASS, SUN_RADIUS);
         AlternativeBody earth = new AlternativeBody("Earth", new Vector(1000000000.0, 500000000.0, 0),
                 earthVelocity, EARTH_MASS, EARTH_RADIUS);
-        List<AlternativeBody> miniverse = new ArrayList<>();
-        miniverse.add(sun);
-        miniverse.add(earth);
+        List<AlternativeBody> miniVerse = new ArrayList<>();
+        miniVerse.add(sun);
+        miniVerse.add(earth);
 
-        Vector testAccel = earth.compute_acceleration(miniverse);
+        Vector testAccel = earth.compute_acceleration(miniVerse);
 
         System.out.println(testAccel);
     }
 
+    @Test
+    void dependentSystemTest() throws CloneNotSupportedException {
+        final double SUN_MASS = 1.989e30;
+        final double SUN_RADIUS = 695700000.0;
+        double EARTH_X0 = -147_095_000_000.0;
+        double EARTH_Y0 = 0.0;
+        Vector earthPosition = new Vector(EARTH_X0, EARTH_Y0, 0);
+        double EARTH_VX0 = 0.0;
+        double EARTH_VY0 = -30300.0;
+        Vector earthVelocity = new Vector(EARTH_VX0, EARTH_VY0, 0);
+        final double EARTH_MASS = 5.972e24;
+        final double EARTH_RADIUS = 6371000.0;
+        final double dt = 86400.0; // 1 earth day in seconds
+
+        final Vector MERCURY_POS0 = new Vector(-46000000000., 0, 0);
+        final Vector VENUS_POS0 = new Vector(-107480000000., 0, 0);
+        final Vector MARS_POS0 = new Vector(-206620000000., 0, 0);
+        final Vector JUPITER_POS0 = new Vector(-740520000000., 0, 0);
+        final Vector SATURN_POS0 = new Vector(-1352550000000., 0, 0);
+        final Vector URANUS_POS0 = new Vector(-2741300000000., 0, 0);
+        final Vector NEPTUNE_POS0 = new Vector(-4444450000000., 0, 0);
+
+        final Vector MERCURY_VEL0 = new Vector(0., -58980, 0);
+        final Vector VENUS_VEL0 = new Vector(0., -35260, 0);
+        final Vector MARS_VEL0 = new Vector(0., -26500, 0);
+        final Vector JUPITER_VEL0 = new Vector(0., -13720, 0);
+        final Vector SATURN_VEL0 = new Vector(0., -10180, 0);
+        final Vector URANUS_VEL0 = new Vector(0., -7110, 0);
+        final Vector NEPTUNE_VEL0 = new Vector(0., -5500, 0);
+
+        final double MERCURY_MASS = .3301e24;//kilograms
+        final double VENUS_MASS = 4.8675e24;
+        final double MARS_MASS = 6.4171e23;
+        final double JUPITER_MASS = 1898.19e24;
+        final double SATURN_MASS = 568.34e24;
+        final double URANUS_MASS = 86.813e24;
+        final double NEPTUNE_MASS = 102.413e24;
+
+        final double MERCURY_RADIUS = 2439700;//meters
+        final double VENUS_RADIUS = 6051800;
+        final double MARS_RADIUS = 3389500;
+        final double JUPITER_RADIUS = 71492000;
+        final double SATURN_RADIUS = 54364000;
+        final double URANUS_RADIUS = 24973000;
+        final double NEPTUNE_RADIUS = 24341000;
+
+
+        AlternativeBody sun = new AlternativeBody("Sun", new Vector(0, 0, 0),
+                new Vector(0, 0, 0), SUN_MASS, SUN_RADIUS);
+        AlternativeBody earth = new AlternativeBody("Earth", earthPosition, earthVelocity, EARTH_MASS, EARTH_RADIUS);
+        AlternativeBody mercury = new AlternativeBody("Mercury", MERCURY_POS0, MERCURY_VEL0, MERCURY_MASS, MERCURY_RADIUS);
+        AlternativeBody venus = new AlternativeBody("Venus", VENUS_POS0, VENUS_VEL0, VENUS_MASS, VENUS_RADIUS);
+        AlternativeBody mars = new AlternativeBody("Mars", MARS_POS0, MARS_VEL0, MARS_MASS, MARS_RADIUS);
+        AlternativeBody jupiter = new AlternativeBody("Jupiter", JUPITER_POS0, JUPITER_VEL0, JUPITER_MASS, JUPITER_RADIUS);
+        AlternativeBody saturn = new AlternativeBody("Saturn", SATURN_POS0, SATURN_VEL0, SATURN_MASS, SATURN_RADIUS);
+        AlternativeBody uranus = new AlternativeBody("Uranus", URANUS_POS0, URANUS_VEL0, URANUS_MASS, URANUS_RADIUS);
+        AlternativeBody neptune = new AlternativeBody("Neptune", NEPTUNE_POS0, NEPTUNE_VEL0, NEPTUNE_MASS, NEPTUNE_RADIUS);
+
+        List<AlternativeBody> solarSystem = new ArrayList<>();
+        solarSystem.add(earth);
+        solarSystem.add(mercury);
+        solarSystem.add(mars);
+        solarSystem.add(venus);
+        solarSystem.add(jupiter);
+        solarSystem.add(saturn);
+        solarSystem.add(uranus);
+        solarSystem.add(neptune);
+        solarSystem.add(sun);
+
+        // simulate 1 earth year for all planets interacting with the sun only
+
+        for (int i = 0; i <= 366; i++) {
+
+            for (AlternativeBody planet : solarSystem
+            ) {
+                System.out.println("day " + i + ", " + planet.name + ". x: " + planet.position.getX() +
+                        ", y: " + planet.position.getY() + ", z:" + planet.position.getZ());
+                planet.step(dt, solarSystem);
+
+            }
+
+        }
+
+    }
 }
